57e6e7aac0d27fbf6468bf6b0aaf831c1f357694
shifa-shereef <shifashareef987@gmai.com>
2026-05-26 09:35:02 +0530

    error code

--- Assets/WeldingGrader.cs
+++ Assets/WeldingGrader.cs
@@ -1,31 +1,7 @@
 using UnityEngine;
 using TMPro; // We will use this for the UI Text later
 
-public class WeldingGrader : MonoBehaviour
-{
-    [Header("Tracking Targets")]
-    public Transform torchTip; // We will drag your torch here tomorrow
-    public TextMeshProUGUI scoreDisplay;
-
-    [Header("Weld Settings")]
-    public float perfectSpeedMMS = 7.5f; // Perfect speed in mm per second
-    public float weldLengthMeters = 0.123f; // The length of your physical block
-
-    private bool isWelding = false;
-    private float startTime;
-
-    // This triggers when the torch enters the invisible Box Collider
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Torch"))
-        {
-            isWelding = true;
-            startTime = Time.time;
-            scoreDisplay.text = "STATUS: WELDING...";
-            scoreDisplay.color = Color.yellow;
-        }
-    }
-
+p
     // This triggers when the torch reaches the end and leaves the Box Collider
     private void OnTriggerExit(Collider other)
     {
